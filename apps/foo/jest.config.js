e366a2d9becf16f4a6e15422fb10ca842cac8282
Alex Wayne <482+AlexJWayne@users.noreply.github.com>
2021-09-09 12:29:22 -0700

    Use recommended transformIgnorePatterns

--- apps/foo/jest.config.js
+++ apps/foo/jest.config.js
@@ -11,4 +11,7 @@ module.exports = {
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/apps/foo',
+  transformIgnorePatterns: [
+    "node_modules/(?!(jest-)?react-native|react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|@sentry/.*)"
+  ]
 };
